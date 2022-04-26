c0ea83d085b481cf8bc03a962c2de8c8c2cc6553
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-04-26 10:47:28 -0400

    rearranged file tree, updated refs.terrain

--- src/contract/address.js
+++ src/contract/address.js
@@ -1,3 +1,3 @@
 import config from "../refs.terrain.json"
 
-export const contractAddress = (wallet) => config[wallet.network.name].clicker.contractAddress.default
+export const contractAddress = (wallet) => config[wallet.network.name].clicker.contractAddresses.default
--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,5 +1,21 @@
-const Leaderboard = () => {
+import * as query from '../contract/query';
+import { useConnectedWallet } from '@terra-money/wallet-provider';
 
-}
+const Leaderboard = () => {
+    const connectedWallet = useConnectedWallet();
+
+    const fetchScores = async () => {
+        if (connectedWallet && connectedWallet.network.name === 'testnet') {
+            console.log("Scores fetched", (await query.getScores(connectedWallet)).scores);
+        }
+    };
+
+    fetchScores();
+
+    return (
+        <>
+        </>
+    );
+};
 
 export default Leaderboard;
