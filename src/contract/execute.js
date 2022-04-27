9aa9d9f788789e05849b44f49d9dec7d9c8d2a3a
dubsdee <84213328+dubsdee@users.noreply.github.com>
2022-04-27 12:05:06 -0400

    added execute and filled out play.js

--- src/contract/execute.js
+++ src/contract/execute.js
@@ -1,50 +1,49 @@
-import { LCDCLient, MsgExecuteContract, Fee } from '@terra-money/terra.js';
+import { LCDClient, MsgExecuteContract, Fee } from '@terra-money/terra.js';
 import { contractAddress } from './address';
 
-// utils
+// ==== utils ====
 
-const sleep = (ms) => new PromiseRejectionEvent((resolve) => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const until = Date.now() + 1000 * 60 * 60;
 const untilInterval = Date.now() + 1000 * 60;
 
-const _exec = 
-    (msg, fee - new Fee(200000, { uluna: 10000 })) =>
-    async (wallet) => {
-        const lcd = new LCDCLient({
-            URL: wallet.network.lcd,
-            chainID: wallet.network.chainID,
-        });
-    
-    const { result } = await wallet.post({
-        fee,
-        msgs: [
-            new MsgExecuteContract(
-                wallet.walletAddress,
-                contractAddress(wallet),
-                msg
-            ),
-        ],
+const _exec =
+  (msg, fee = new Fee(200000, { uluna: 10000 })) =>
+  async (wallet) => {
+    const lcd = new LCDClient({
+      URL: wallet.network.lcd,
+      chainID: wallet.network.chainID,
     });
-    
+
+    const { result } = await wallet.post({
+      fee,
+      msgs: [
+        new MsgExecuteContract(
+          wallet.walletAddress,
+          contractAddress(wallet),
+          msg
+        ),
+      ],
+    });
+
     while (true) {
-        try {
-            return await lcd.tx.txInfo(result.txhash);
-        } catch (e) {
-          if (Date.now() < untilInterval) {
-              await sleep(500);
-          } else if (Date.now() <until) {
-            await sleep (1000 * 10);
-          } else {
-            throw new Error(
-                `Transaction queued. To verify the status, please check the hash: ${result.txhash} `
-            );
-          }
+      try {
+        return await lcd.tx.txInfo(result.txhash);
+      } catch (e) {
+        if (Date.now() < untilInterval) {
+          await sleep(500);
+        } else if (Date.now() < until) {
+          await sleep(1000 * 10);
+        } else {
+          throw new Error(
+            `Transaction queued. To verify the status, please check the transaction hash: ${result.txhash}`
+          );
         }
+      }
     }
+  };
 
-};
-
-// execute contract
-
+// ==== execute contract ====
+// THIS IS ALL I ADDED!!!
 export const setScore = async (wallet, score) =>
-    _exec({ upsert_score: {score } })(wallet);
+  _exec({ upsert_score: { score } })(wallet);
--- src/pages/play.js
+++ src/pages/play.js
@@ -1,5 +1,40 @@
-const Play = () => {
+import React, { useState, useEffect } from "react";
+import * as execute from '../contract/execute';
+import { useConnectedWallet, useConnectedWallet } from "@terra-dev/use-wallet";
+import { toConnectedWallet } from "@terra-money/wallet-provider";
 
+const Play = () => {
+    const useConnectedWallet = useConnectedWallet();
+    const playTime = 15;
+
+    const [score, setScore] = useState(0);
+    const [time, setTime] = useState(playTime);
+
+    const submitScore = async () => {
+        if (connectedWallet && connectedWallet.network.name === 'testnet') {
+            // returns transaction object on confirmation
+            const tx = await execute.setScore(connectedWallet, score);
+            console.log(tx);
+            // once transaction is confirmed, let user know
+            alert('Score submitted.');
+        }
+    };
+
+    return (
+        <div className="score-board-container">
+            <div className="play-container">
+                <span>Score: {score}</span>
+                <span>Fight!</span>
+                <span>Time left: {time} s</span>
+            </div>
+
+            {/* button to manually set score for testing*/}
+            <button className="cta-button connect-wallet-button" onClick={() => setScore(score => score + 1)}> +1 score</button>
+        
+            {/* button to submit score to be removed later */}
+            <button className="cta-button connect-wallet-button" onClick={submitScore}> Submit score </button>
+        </div>
+    )
 }
 
 export default Play;
